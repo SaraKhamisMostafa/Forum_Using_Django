61fd39be79dbe22bc86c27cfa0cbbe9610d5e339
SaraKhamisMostafa <sarakhamismostafa@gmail.com>
2023-06-19 17:03:50 +0300

    add str method, add data into daatbase

---
+++ forum/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+    question=models.CharField(max_length=300)
+    author=models.CharField(max_length=50)
+    create_date=models.DateTimeField(default=timezone.now)
+
+class Answer(models.Model):
+    question=models.ForeignKey(Question,related_name='question_answer',on_delete=models.CASCADE)
+    answer=models.TextField(max_length=3000)
+    author=models.CharField(max_length=50)
+    create_date=models.DateTimeField(default=timezone.now)
